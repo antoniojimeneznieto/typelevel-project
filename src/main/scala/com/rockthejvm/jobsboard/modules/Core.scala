7b47e03b787ed1b22005e8cb9ecb1cd286b1806c
antjim1 <antonio.jimeneznieto@epfl.ch>
2023-08-08 00:26:20 +0200

    Move the transactor to a new object

--- src/main/scala/com/rockthejvm/jobsboard/modules/Core.scala
+++ src/main/scala/com/rockthejvm/jobsboard/modules/Core.scala
@@ -7,25 +7,12 @@ import doobie.*
 import doobie.util.*
 import doobie.hikari.HikariTransactor
 
-final class Core[F[_]: Sync] private (val jobs: Jobs[F]) {
-  
-}
+final class Core[F[_]: Sync] private (val jobs: Jobs[F]) {}
 
 // postgres -> jobs -> core -> httpApi -> app
 object Core {
-  def postgresResource[F[_]: Async]: Resource[F, HikariTransactor[F]] = for {
-    ec <- ExecutionContexts.fixedThreadPool(32)
-    xa <- HikariTransactor.newHikariTransactor[F](
-      "org.postgresql.Driver",
-      "jdbc:postgresql:board", // TODO move to config
-      "docker",
-      "docker",
-      ec
-    )
-  } yield xa
-
-  def apply[F[_]: Async]: Resource[F, Core[F]] = 
-    postgresResource[F]
-      .evalMap(postgres => LiveJobs[F](postgres))
+  def apply[F[_]: Async](xa: Transactor[F]): Resource[F, Core[F]] =
+    Resource
+      .eval(LiveJobs[F](xa))
       .map(jobs => new Core(jobs))
 }
---
+++ src/main/scala/com/rockthejvm/jobsboard/modules/Database.scala
@@ -0,0 +1,19 @@
+package com.rockthejvm.jobsboard.modules
+
+import cats.effect.*
+import doobie.util.*
+import doobie.hikari.HikariTransactor
+import com.rockthejvm.jobsboard.config.*
+
+object Database {
+  def makePostgresResource[F[_]: Async](config: PostgresConfig): Resource[F, HikariTransactor[F]] = for {
+    ec <- ExecutionContexts.fixedThreadPool(config.nThreads)
+    xa <- HikariTransactor.newHikariTransactor[F](
+      "org.postgresql.Driver",
+      config.url,
+      config.user,
+      config.pass,
+      ec
+    )
+  } yield xa
+}
