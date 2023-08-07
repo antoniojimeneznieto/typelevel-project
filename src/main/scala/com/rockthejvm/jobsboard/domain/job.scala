3064ebc6680ecafef9fc7637cd9b892acb0ab0c6
antjim1 <antonio.jimeneznieto@epfl.ch>
2023-08-07 18:13:30 +0200

    Add minimal constructor for JobInfo

--- src/main/scala/com/rockthejvm/jobsboard/domain/job.scala
+++ src/main/scala/com/rockthejvm/jobsboard/domain/job.scala
@@ -4,32 +4,50 @@ import java.util.UUID
 
 object job {
   case class Job(
-    id: UUID,
-    date: Long, 
-    ownerEmail: String,
-    jobInfo: JobInfo,
-    active: Boolean = false
+      id: UUID,
+      date: Long,
+      ownerEmail: String,
+      jobInfo: JobInfo,
+      active: Boolean = false
   )
 
   case class JobInfo(
-    company: String,
-    title: String,
-    description: String,
-    externalUrl: String,
-    remote: Boolean, 
-    location: String,
-    salaryLo: Option[Int],
-    salaryHi: Option[Int],
-    currency: Option[String],
-    country: Option[String],
-    tags: Option[List[String]],
-    image: Option[String],
-    seniority: Option[String],
-    other: Option[String]
+      company: String,
+      title: String,
+      description: String,
+      externalUrl: String,
+      remote: Boolean,
+      location: String,
+      salaryLo: Option[Int],
+      salaryHi: Option[Int],
+      currency: Option[String],
+      country: Option[String],
+      tags: Option[List[String]],
+      image: Option[String],
+      seniority: Option[String],
+      other: Option[String]
   )
 
   object JobInfo {
     val empty: JobInfo =
-        JobInfo("", "", "", "", false, "", None, None, None, None, None, None, None, None)
+      JobInfo("", "", "", "", false, "", None, None, None, None, None, None, None, None)
+
+    def minimal(
+        company: String,
+        title: String,
+        description: String,
+        externalUrl: String,
+        remote: Boolean,
+        location: String
+    ): JobInfo = JobInfo(
+      company,
+      title,
+      description,
+      externalUrl,
+      remote,
+      location,
+      None, None, None, None, None, None, None, None
+    )
+
   }
 }
