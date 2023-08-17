46e1a4a88583e3c197546f13ebe7b967e44d6202
antjim1 <antonio.jimeneznieto@epfl.ch>
2023-08-17 12:25:22 +0200

    Add AuthRoute type in security

--- src/main/scala/com/rockthejvm/jobsboard/domain/security.scala
+++ src/main/scala/com/rockthejvm/jobsboard/domain/security.scala
@@ -1,5 +1,6 @@
 package com.rockthejvm.jobsboard.domain
 
+import org.http4s.Response
 import tsec.authentication.*
 import tsec.mac.jca.HMACSHA256
 import com.rockthejvm.jobsboard.domain.user.*
@@ -8,4 +9,5 @@ object security {
   type Crypto = HMACSHA256
   type JwtToken = AugmentedJWT[Crypto, String]
   type Authenticator[F[_]] = JWTAuthenticator[F, String, User, Crypto]
+  type AuthRoute[F[_]] = PartialFunction[SecuredRequest[F, User, JwtToken], F[Response[F]]]
 }
