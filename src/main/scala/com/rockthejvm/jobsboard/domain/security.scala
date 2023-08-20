9e0a0bb1e05acefea929497407423fb620e69dc8
antjim1 <antonio.jimeneznieto@epfl.ch>
2023-08-21 01:01:48 +0200

    Abstract the authenticator and SecureHandler for jobs and users

--- src/main/scala/com/rockthejvm/jobsboard/domain/security.scala
+++ src/main/scala/com/rockthejvm/jobsboard/domain/security.scala
@@ -19,7 +19,9 @@ object security {
   type JwtToken            = AugmentedJWT[Crypto, String]
   type Authenticator[F[_]] = JWTAuthenticator[F, String, User, Crypto]
   type AuthRoute[F[_]]     = PartialFunction[SecuredRequest[F, User, JwtToken], F[Response[F]]]
+  // type aliasis for http route
   type AuthRBAC[F[_]]      = BasicRBAC[F, Role, User, JwtToken]
+  type SecureHandler[F[_]] = SecuredRequestHandler[F, String, User, JwtToken]
 
   // RBAC
   // BasicRBAC[F, Role, User, JwtToken]
