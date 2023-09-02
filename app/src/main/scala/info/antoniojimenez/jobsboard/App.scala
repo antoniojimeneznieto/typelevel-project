cfef68ef3c994f6b621aa883bcde54aaf69e3851
antjim1 <antonio.jimeneznieto@epfl.ch>
2023-09-02 18:03:01 +0200

    Add jobs, signup, login

--- app/src/main/scala/info/antoniojimenez/jobsboard/App.scala
+++ app/src/main/scala/info/antoniojimenez/jobsboard/App.scala
@@ -4,16 +4,18 @@ import tyrian.*
 import tyrian.Html.*
 import cats.effect.*
 import scala.scalajs.js.annotation.*
-import org.scalajs.dom.{document, console}
+import org.scalajs.dom.window
 import scala.concurrent.duration.*
 import tyrian.cmds.Logger
 
-object App {
-  sealed trait Msg
-  case class Increment(amount: Int) extends Msg
+import core.*
 
-  case class Model(count: Int)
+object App {
+  type Msg = Router.Msg
+
+  case class Model(router: Router)
 }
+
 @JSExportTopLevel("antonioApp")
 class App extends TyrianApp[App.Msg, App.Model] { // [Message, model = "state"]
   import App.*
@@ -23,30 +25,49 @@ class App extends TyrianApp[App.Msg, App.Model] { // [Message, model = "state"]
     - create a subscription
     - listening for an event
    */
-  override def init(flags: Map[String, String]): (Model, Cmd[IO, Msg]) =
-    (Model(0), Cmd.None)
+  override def init(flags: Map[String, String]): (Model, Cmd[IO, Msg]) = {
+    val (router, cmd) = Router.startAt(window.location.pathname)
+    (Model(router), cmd)
+  }
 
   // potentially endless stream of messages
   override def subscriptions(model: Model): Sub[IO, Msg] =
-    // Sub.None
-    Sub.every[IO](1.second).map(_ => Increment(1))
+    Sub.make( // listener for browser history changes
+      "urlChange",
+      model.router.history.state.discrete
+        .map(_.get)
+        .map(newLocation => Router.ChangeLocation(newLocation, true))
+    )
 
   // model can change by receiving messages
   // model => messaage => (new model, _)
   // update triggered whenever we get a new message
   override def update(model: Model): Msg => (Model, Cmd[IO, Msg]) =
-    case Increment(amount) => {
-      (
-        model.copy(count = model.count + amount),
-        Logger.consoleLog[IO]("Changing count by " + amount)
-      )
-    }
+    case msg: Router.Msg =>
+      val (newRouter, cmd) = model.router.update(msg)
+      (model.copy(router = newRouter), cmd)
+
   // view triggered whenever model changes
   override def view(model: Model): Html[Msg] =
     div(
-      button(onClick(Increment(1)))("increase"),
-      button(onClick(Increment(-1)))("decrease"),
-      div(s"Tyrian running: ${model.count}")
+      // <a href="/jobs">Jobs</a>
+      renderNavLink("Jobs", "/jobs"),
+      renderNavLink("Login", "/login"),
+      renderNavLink("Sign up", "/signup"),
+      div(s"You are now at: ${model.router.location}")
     )
+
+  private def renderNavLink(text: String, location: String) =
+    a(
+      href    := location,
+      `class` := "nav-link",
+      onEvent(
+        "click",
+        e => {
+          e.preventDefault() // prevent relaoding the page
+          Router.ChangeLocation(location)
+        }
+      )
+    )(text)
 
 }
---
+++ app/src/main/scala/info/antoniojimenez/jobsboard/core/Router.scala
@@ -0,0 +1,36 @@
+package info.antoniojimenez.jobsboard.core
+
+import tyrian.*
+import cats.effect.*
+import fs2.dom.History
+
+// antoniojimenez.info/location
+case class Router private (location: String, history: History[IO, String]) {
+  import Router.*
+
+  def update(msg: Msg): (Router, Cmd[IO, Msg]) = msg match {
+    case ChangeLocation(newLocation, browserTriggered) =>
+      if (location == newLocation) (this, Cmd.None)
+      else {
+        val historyCmd =
+          if (browserTriggered) Cmd.None
+          else goto(newLocation)
+        (this.copy(location = newLocation), historyCmd)
+      }
+    case _ => (this, Cmd.None)
+  }
+
+  def goto[M](location: String): Cmd[IO, M] = Cmd.SideEffect[IO] {
+    history.pushState(location, location)
+  }
+}
+
+object Router {
+  trait Msg
+  case class ChangeLocation(location: String, browserTriggered: Boolean = false) extends Msg
+  case class ExternalRedirect(location: String)                                  extends Msg
+
+  def startAt[M](initialLocation: String): (Router, Cmd[IO, M]) =
+    val router = Router(initialLocation, History[IO, String])
+    (router, router.goto(initialLocation))
+}
