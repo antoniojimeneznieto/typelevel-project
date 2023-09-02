723ee64b0bf27e3df268e3150d918fb1d109f336
antjim1 <antonio.jimeneznieto@epfl.ch>
2023-09-02 14:32:49 +0200

    playing with tyrian

--- app/src/main/scala/info/antoniojimenez/jobsboard/App.scala
+++ app/src/main/scala/info/antoniojimenez/jobsboard/App.scala
@@ -1,14 +1,52 @@
 package info.antoniojimenez.jobsboard
 
-import scala.scalajs.js.annotation.* 
-import org.scalajs.dom.document
+import tyrian.*
+import tyrian.Html.*
+import cats.effect.*
+import scala.scalajs.js.annotation.*
+import org.scalajs.dom.{document, console}
+import scala.concurrent.duration.*
+import tyrian.cmds.Logger
 
+object App {
+  sealed trait Msg
+  case class Increment(amount: Int) extends Msg
+
+  case class Model(count: Int)
+}
 @JSExportTopLevel("antonioApp")
-class App {
-  @JSExport
-  def doSomething(containerId: String) = 
-    document.getElementById(containerId).innerHTML = "THIS ROCKS"
+class App extends TyrianApp[App.Msg, App.Model] { // [Message, model = "state"]
+  import App.*
+  /*
+  We can send messages by
+    - trigger a command
+    - create a subscription
+    - listening for an event
+   */
+  override def init(flags: Map[String, String]): (Model, Cmd[IO, Msg]) =
+    (Model(0), Cmd.None)
 
-  //
-  
+  // potentially endless stream of messages
+  override def subscriptions(model: Model): Sub[IO, Msg] =
+    // Sub.None
+    Sub.every[IO](1.second).map(_ => Increment(1))
+
+  // model can change by receiving messages
+  // model => messaage => (new model, _)
+  // update triggered whenever we get a new message
+  override def update(model: Model): Msg => (Model, Cmd[IO, Msg]) =
+    case Increment(amount) => {
+      (
+        model.copy(count = model.count + amount),
+        Logger.consoleLog[IO]("Changing count by " + amount)
+      )
+    }
+  // view triggered whenever model changes
+  override def view(model: Model): Html[Msg] =
+    div(
+      button(onClick(Increment(1)))("increase"),
+      button(onClick(Increment(-1)))("decrease"),
+      div(s"Tyrian running: ${model.count}")
+    )
+
 }
